e719ba413c315c2d1d76ad4f46e1ee508d2f4059
Kohei Okamoto <okaponta.19910919@gmail.com>
2025-03-22 00:08:15 +0900

    add 4-1

---
+++ chapter4/4-1.rb
@@ -0,0 +1,24 @@
+require "minruby"
+
+def evaluate(tree)
+  if tree[0] == "lit"
+    return tree[1]
+  end
+  left = evaluate(tree[1])
+  right = evaluate(tree[2])
+  case tree[0]
+  when "+"
+    left + right
+  when "-"
+    left - right
+  when "*"
+    left * right
+  else
+    left / right
+  end
+end
+
+str = gets
+tree = minruby_parse(str)
+answer = evaluate(tree)
+p(answer)
